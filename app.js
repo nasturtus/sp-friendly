a827b5153b8cc7b04086c19d5c95f499bfba2193
nasturtus <nasturtus@gmail.com>
2018-05-28 18:03:18 +0800

    Add showAll, clearAll, manageFriends routes to app middleware

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
 
 // declare app as an instance of express
 const app = express();
 
+// declare and use logger
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// declare and use body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 // declare middleware
-const friendsRouter = require("./route/friends");
-app.use("/", friendsRouter);
+const welcomeRouter = require("./route/welcome");
+app.use("/", welcomeRouter);
+
+const showAllRouter = require("./route/showAll");
+app.use("/showall", showAllRouter);
+
+const clearAllRouter = require("./route/clearAll");
+app.use("/clearall", clearAllRouter);
+
+const manageFriendsRouter = require("./route/manageFriends");
+app.use("/managefriends", manageFriendsRouter);
 
 module.exports = app;
