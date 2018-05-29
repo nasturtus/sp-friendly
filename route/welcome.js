6865ea5f3dee9637cf4598345e3e345c359d469b
nasturtus <nasturtus@gmail.com>
2018-05-29 20:07:51 +0800

    Amend menu options.

--- route/welcome.js
+++ route/welcome.js
@@ -9,12 +9,12 @@ welcomeRouter.get("/", (req, res) => {
       "/showall": "GET: show all documents in collection",
       "/clearall": "POST: clears the collection of all documents",
       "/adduser": "POST: add a new user",
-      "/managefriends/addfriends": "POST: add friends",
+      "/managefriends/addfriends": "PUT: add friends",
       "/managefriends/getfriends": "GET: get friends for a user",
       "/managefriends/getcommonfriends": "GET: get friends common to two users",
       "/managefriends/subscribe":
-        "POST: subscribe to updates from another user",
-      "/managefriends/block": "POST: block updates from another user",
+        "PUT: subscribe to updates from another user",
+      "/managefriends/block": "PUT: block updates from another user",
       "/managefriends/getrecipients":
         "GET: get users who'll receive updates from sender"
     }
